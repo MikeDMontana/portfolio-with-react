3e69633227591c658be54ff949369291f687aceb
Mike Dreiling <mdarts@gmail.com>
2017-04-20 13:32:40 -0600

    working portfolio section using bootstrap

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/*  var findUser = function(name, dataArr) {
+}
+
+$.ajax({
+  'url' : 'https://pixabay.com/api/?key=5055067-78f0d6038ffd8607834757e55&q=yellow+flowers&image_type=photo',
+  'type' : 'GET',
+  'success' : function(data) {
+    for (var i=0; i< data.hits.length; i++) {
+      if(data.hits[i].user === "Josch13") {
+      console.log(data.hits[i].webformatURL);
+      }
+    }
+
+    //create function that takes in a string and assigns string to name of the user
+    //might use unshift if using return array
+    //$("#intro").append('<img src="' + data.hits[0].webformatURL + '">');
+   },
+});  */
